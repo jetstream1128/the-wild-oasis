451d2da054665b14b347804c8fca9edb60f1c4e4
jetstream1128 <bogdanalexeev2007@live.com>
2024-05-18 21:17:25 -0600

    setting up the rotes

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,32 @@
-import styled from "styled-components";
+import { Navigate, Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import Settings from "./pages/Settings";
+import NewUsers from "./pages/Users";
+import PageNotFound from "./pages/PageNotFound";
 import GlobalStyles from "./styles/GlobalStyles";
-import Input from "./ui/Input";
-import Button from "./ui/Button";
-import Heading from "./ui/Hading";
-import Row from "./ui/Row";
-
-const StyledApp = styled.main`
-	padding: 20px;
-`;
 
 function App() {
 	return (
 		<>
 			<GlobalStyles />
-			<StyledApp>
-				<Row type="vertical">
-					<Row type="horizontal">
-						<Heading as="h1">The wild Oasis</Heading>
-
-						<div>
-							<Heading as="h2">Check in and out</Heading>
-							<Button onClick={() => alert("Check in")}>Check in</Button>
-							<Button
-								variation="secondary"
-								size="small"
-								onClick={() => alert("Check out")}
-							>
-								Check out
-							</Button>
-						</div>
-					</Row>
-					<Row type="vertical">
-						<Heading as="h3">Form</Heading>
-						<form>
-							<Input type="number" placeholder="Number of guests" />
-							<Input type="number" placeholder="Number of guests" />
-						</form>
-					</Row>
-				</Row>
-			</StyledApp>
+			<BrowserRouter>
+				<Routes>
+					<Route index element={<Navigate replace to="dashboard" />} />
+					<Route path="dashboard" element={<Dashboard />} />
+					<Route path="bookings" element={<Bookings />} />
+					<Route path="cabins" element={<Cabins />} />
+					<Route path="account" element={<Account />} />
+					<Route path="login" element={<Login />} />
+					<Route path="settings" element={<Settings />} />
+					<Route path="users" element={<NewUsers />} />
+					<Route path="*" element={<PageNotFound />} />
+				</Routes>
+			</BrowserRouter>
 		</>
 	);
 }
