033eb8ab75032a24f450d2f6b719282fd80b35a3
jetstream1128 <bogdanalexeev2007@live.com>
2024-05-18 18:13:44 -0600

    Styled buttons

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,8 @@ import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Input from "./ui/Input";
 import Button from "./ui/Button";
-
-const H1 = styled.h1`
-	font-size: 30px;
-	font-weight: 600;
-	text-align: center;
-`;
+import Heading from "./ui/Hading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.main`
 	padding: 20px;
@@ -18,11 +14,30 @@ function App() {
 		<>
 			<GlobalStyles />
 			<StyledApp>
-				<H1>Hello world</H1>
-				<Button onClick={() => alert("Check in")}>Check in</Button>
-				<Button onClick={() => alert("Check out")}>Check out</Button>
+				<Row type="vertical">
+					<Row type="horizontal">
+						<Heading as="h1">The wild Oasis</Heading>
 
-				<Input type="number" placeholder="Number of guests" />
+						<div>
+							<Heading as="h2">Check in and out</Heading>
+							<Button onClick={() => alert("Check in")}>Check in</Button>
+							<Button
+								variation="secondary"
+								size="small"
+								onClick={() => alert("Check out")}
+							>
+								Check out
+							</Button>
+						</div>
+					</Row>
+					<Row type="vertical">
+						<Heading as="h3">Form</Heading>
+						<form>
+							<Input type="number" placeholder="Number of guests" />
+							<Input type="number" placeholder="Number of guests" />
+						</form>
+					</Row>
+				</Row>
 			</StyledApp>
 		</>
 	);
